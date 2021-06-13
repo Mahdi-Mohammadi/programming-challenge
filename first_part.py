abf8e152150f2e4c8cdbe3a63d25cbce195cce92
Mahdi Mohammadi <mahdi.mohammadi@whipmobility.com>
2021-06-13 18:56:02 +0800

    Add space function with tests

--- first_part.py
+++ first_part.py
@@ -14,3 +14,9 @@ def get_alphanumeric(min_length: int = 2, max_length: int = 10) -> str:
     if not any(map(str.isalpha, alphanumeric)):
         alphanumeric = get_alphanumeric(min_length, max_length)
     return alphanumeric
+
+
+def get_space(input_string: str, min_length, max_length):
+    before_space_length = random.randint(min_length, max_length - 1)
+    after_space_length = random.randint(1, max_length - before_space_length)
+    return ' ' * before_space_length + input_string + ' ' * after_space_length
--- test_first_part_unittest.py
+++ test_first_part_unittest.py
@@ -2,6 +2,7 @@ import unittest
 import random
 from first_part import get_alpha
 from first_part import get_alphanumeric
+from first_part import get_space
 
 
 class TestAlpha(unittest.TestCase):
@@ -24,11 +25,11 @@ class TestAlpha(unittest.TestCase):
 
 class TestAlphanumeric(unittest.TestCase):
     def test_output_type(self):
-        output_len = random.randint(1, 20)
+        output_len = random.randint(2, 20)
         self.assertIsInstance(get_alphanumeric(output_len, output_len), str, f"Output should be {str}")
 
     def test_length(self):
-        output_len = random.randint(1, 20)
+        output_len = random.randint(2, 20)
         self.assertEqual(len(get_alphanumeric(output_len, output_len)), output_len, f"Length should be {output_len}")
 
     def test_random_length(self):
@@ -43,6 +44,23 @@ class TestAlphanumeric(unittest.TestCase):
     def test_digit_exist(self):
         self.assertTrue(any(map(str.isdigit, get_alphanumeric(2, 2))),
                         "Output should be contains at least one digit char")
+
+
+class TestSpace(unittest.TestCase):
+    def test_output_type(self):
+        output_len = random.randint(2, 20)
+        self.assertIsInstance(get_space('A', 2, output_len), str, f"Output should be {str}")
+
+    def test_random_length(self):
+        output_len = random.randint(10, 20)
+        self.assertTrue(3 <= len(get_space('A', 2, output_len)) <= output_len + 1,
+                        f"Length should be random and between 3 and {output_len + 1}")
+
+    def test_before_space_exist(self):
+        self.assertTrue(get_space('A', 2, 20)[0] == ' ', "First char must be space")
+
+    def test_after_space_exist(self):
+        self.assertTrue(get_space('A', 2, 20)[-1] == ' ', "Last char must be space")
 
 
 if __name__ == '__main__':

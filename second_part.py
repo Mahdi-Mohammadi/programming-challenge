abdbc663e325aa5af92c41dbf6ccbab97199ef8b
Mahdi Mohammadi <mahdi.mohammadi@whipmobility.com>
2021-06-14 13:22:19 +0800

    Init second part

---
+++ second_part.py
@@ -0,0 +1,30 @@
+import os
+import yaml
+
+
+def is_alpha(input_string: str) -> bool:
+    pass
+
+
+def is_alphanumeric(input_string: str) -> bool:
+    pass
+
+
+def is_int(input_string: str) -> bool:
+    pass
+
+
+def is_float(input_string: str) -> bool:
+    pass
+
+
+def check_random_file(file_path: str) -> bool:
+    pass
+
+
+if __name__ == '__main__':
+    config = yaml.safe_load(open("config.yml"))
+    file_path = config['result']['path']
+    if not os.path.exists(file_path):
+        raise FileExistsError(f"File '{file_path}' does not exist")
+    check_random_file(file_path)
---
+++ test_second_part_unittest.py
@@ -0,0 +1,12 @@
+import os
+import yaml
+import random
+import unittest
+from second_part import is_alpha
+from second_part import is_alphanumeric
+from second_part import is_int
+from second_part import is_float
+from second_part import check_random_file
+
+if __name__ == '__main__':
+    unittest.main()

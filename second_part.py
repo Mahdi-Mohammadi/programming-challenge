3f9088456541de390fe2bd3b115a7efa93cd99db
Mahdi Mohammadi <mahdi.mohammadi@whipmobility.com>
2021-06-15 14:18:35 +0800

    Add float check function

--- second_part.py
+++ second_part.py
@@ -6,13 +6,17 @@ def is_alpha(input_string: str) -> bool:
     return input_string.isalpha()
 
 
-
 def is_int(input_string: str) -> bool:
-    return input_string.isnumeric()
+    return input_string.isdigit()
 
 
 def is_float(input_string: str) -> bool:
-    pass
+    if '.' not in input_string:
+        return False
+    num = input_string.split('.')
+    if num[-1] is not None and num[-1].isdigit():
+        return True
+    return False
 
 
 def check_random_file(file_path: str) -> bool:
--- test_second_part_unittest.py
+++ test_second_part_unittest.py
@@ -34,5 +34,18 @@ class TestInt(unittest.TestCase):
             self.assertFalse(is_int(false_case), false_case)
 
 
+class TestFloat(unittest.TestCase):
+    true_test_cases = ['1.0', '11.1', '111.123456789']
+    false_test_cases = ['AA', '11', '0.', 'a.a', 'Aa@']
+
+    def test_true_cases(self):
+        for true_case in self.true_test_cases:
+            self.assertTrue(is_float(true_case), true_case)
+
+    def test_false_cases(self):
+        for false_case in self.false_test_cases:
+            self.assertFalse(is_float(false_case), false_case)
+
+
 if __name__ == '__main__':
     unittest.main()
